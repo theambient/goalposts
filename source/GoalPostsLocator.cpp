6b3c57db913cae6367484017215e6eac5889d477
Ruslan Mullakhmetov <ruslan@twentythree.net>
2021-05-06 03:05:37 +0200

    implemented  GoalPostsLocator

--- source/GoalPostsLocator.cpp
+++ source/GoalPostsLocator.cpp
@@ -1,5 +1,6 @@
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
 #include "GoalPostsLocator.h"
 #include "utils.h"
 
@@ -22,5 +23,119 @@ std::vector<cv::Point2f> GoalPostsLocator::get_approx_points() const
 
 std::vector<cv::Point2f> GoalPostsLocator::locate()
 {
-	return _approx_points;
+	size_t margin_size = _pattern_size + _search_wnd_size;
+	cv::copyMakeBorder(_img, _img_ext, margin_size, margin_size, margin_size, margin_size, cv::BORDER_REFLECT_101);
+
+	auto pattern = _calc_pattern();
+	std::vector<cv::Point2f> located_pts;
+
+	for(auto& ap: _approx_points)
+	{
+		auto start_point = cv::Point2i(ap.x - _pattern_size / 2, ap.y - _pattern_size / 2);
+		auto pt_matched = _match_pattern(pattern, start_point);
+		auto pt_located = cv::Point2i(pt_matched.x + ap.x - start_point.x, pt_matched.x + ap.y - start_point.y);
+		located_pts.push_back(pt_located);
+	}
+
+	return std::move(located_pts);
+}
+
+cv::Point2i GoalPostsLocator::_match_pattern(cv::Mat pattern, cv::Point2i sp)
+{
+	float val_max = 0;
+	cv::Point2i pt_max = sp;
+
+	for(int y=sp.y-_search_wnd_size; y<sp.y+_search_wnd_size+1; ++y)
+	{
+		for(int x=sp.x-_search_wnd_size; x<sp.x+_search_wnd_size+1; ++x)
+		{
+			float val = _calc_metric(x, y, pattern, sp);
+			if(val > val_max)
+			{
+				val_max = val;
+				pt_max = sp;
+			}
+		}
+	}
+
+	return pt_max;
+}
+
+float GoalPostsLocator::_calc_metric(int x, int y, cv::Mat pattern, cv::Point2i sp)
+{
+	auto pptr = pattern.ptr<unsigned char>(sp.x, sp.y);
+	auto iptr = _img_ext.ptr<unsigned char>(x, y);
+
+	unsigned acc[2] = {0};
+	unsigned cnt[2] = {0};
+
+	int width = pattern.cols;
+	int stride = width - _pattern_size;
+
+	for(int i=0; i<_pattern_size; ++i)
+	{
+		for(int j=0; j<_pattern_size; ++j)
+		{
+			acc[*pptr == 0] += *iptr;
+			cnt[*pptr == 0] += 1;
+			++pptr;
+			++iptr;
+		}
+
+		pptr += stride;
+		iptr += stride;
+	}
+
+	return abs(acc[0] / float(cnt[0]) - acc[1] / float(cnt[1]));
+}
+
+cv::Mat GoalPostsLocator::_calc_pattern()
+{
+    int w = _img_ext.cols;
+    int h = _img_ext.rows;
+
+    cv::Mat pattern = cv::Mat::zeros(h,w, CV_8U);
+
+	int GH = 637;
+	int GW = 1118;
+    int BAR_SIZE = 30;
+    int BS = BAR_SIZE;
+    int BS2 = BAR_SIZE / 2;
+    int BW = 30;
+    int BW2 = BW / 2;
+    cv::Point2i pts[1][8] = {{
+    	cv::Point2i(0, GH),
+    	cv::Point2i(0, 0),
+    	cv::Point2i(GW, 0),
+    	cv::Point2i(GW, GH),
+    	cv::Point2i(GW - BW, GH),
+    	cv::Point2i(GW - BW, BS),
+    	cv::Point2i(BW, BS),
+    	cv::Point2i(BW, GH),
+    }};
+
+    const cv::Point* ppt[1] = { pts[0] };
+    const int npt[] = {8};
+
+    cv::fillPoly(pattern, ppt, npt, 1, cv::Scalar(255,255,255), cv::LINE_8);
+
+    cv::Point2f srcTri[] = {
+        cv::Point2f(BW2, GH),
+        cv::Point2f(BW2, BS2),
+        cv::Point2f(GW - BW2, BS2),
+        cv::Point2f(GW - BW2, GH),
+    };
+
+    cv::Point2f dstTri[] = {
+        _approx_points[0],
+        _approx_points[1],
+        _approx_points[3],
+        _approx_points[4],
+    };
+
+    cv::Mat warp_mat = cv::getPerspectiveTransform(srcTri, dstTri);
+    cv::Mat pattern_w;
+    cv::warpPerspective(pattern, pattern_w, warp_mat, cv::Size(w, h));
+
+    return pattern_w;
 }
--- source/GoalPostsLocator.h
+++ source/GoalPostsLocator.h
@@ -4,7 +4,14 @@
 class GoalPostsLocator
 {
 	cv::Mat _img;
+	cv::Mat _img_ext;
 	std::vector<cv::Point2f> _approx_points;
+	int _pattern_size;
+	int _search_wnd_size;
+
+	cv::Point2i _match_pattern(cv::Mat pattern, cv::Point2i sp);
+	float _calc_metric(int x, int y, cv::Mat pattern, cv::Point2i sp);
+	cv::Mat _calc_pattern();
 public:
 	GoalPostsLocator(std::string img_path, std::string approx_points_path);
 	GoalPostsLocator(cv::Mat img, std::string approx_points_path);
